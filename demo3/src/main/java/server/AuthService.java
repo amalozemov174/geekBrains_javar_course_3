ae616ca1368632a4d685fda6a25ffee6d579266b
Anatoly <glenglen74@gmail.com>
2022-05-23 08:36:59 +0500

    lesson 3 file input/output

---
+++ demo3/src/main/java/server/AuthService.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.io.Closeable;
+
+public interface AuthService extends Closeable {
+    String getNickByLoginAndPassword(String login, String password);
+
+    void start();
+
+    void close();
+
+    void setNickName(String nick, String login);
+}
